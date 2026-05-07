6f9dd61c9dfe0eb4f44cd090a668adfc590cee5d
Qui-0 <93606147+Qui-0@users.noreply.github.com>
2026-05-07 21:02:07 +0900

    Hashtag.java : hashtag 엔티티생성. (#8)
    hashtag_init_1.sql : 타입 NUMBER(10) -> (19)로 변경.
    HashtagDao.java : hashtag Dao 생성.
    HashtagDao.xml : Dao과 연동되는 xml 작성.(codex 활용)
    Post.java : createhashtag() 유틸 추가.
    post_init_1.sql : 타입 변경 위와 동일.
    PostController.java : 불필요한 주석 제거.
    PostCreateRequest.java : 불필요한 주석 제거.
    PostDao.xml : useGeneratedKeys 추가.
    postHashtags_init_1.sql : hashtags db 생성 sql.
    PostServiceImpl.java : hashtags insert 추가.
    user_init_1.sql : 타입 변경 위와 동일.

---
+++ src/main/java/com/bizteam3/server/post/dao/HashtagDao.java
@@ -0,0 +1,16 @@
+package com.bizteam3.server.post.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.Set;
+
+@Mapper
+public interface HashtagDao {
+    int insertHashtag(@Param("hashtagSet") Set<String> hashtagSet);
+
+    int insertPostHashtag(
+            @Param("postId") Integer postId,
+            @Param("hashtagSet") Set<String> hashtagSet
+    );
+}
--- src/main/java/com/bizteam3/server/post/entity/Post.java
+++ src/main/java/com/bizteam3/server/post/entity/Post.java
@@ -1,6 +1,10 @@
 package com.bizteam3.server.post.entity;
 
 import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -22,5 +26,17 @@ public class Post {
 		this.userId = userId;
 		this.caption = caption;
 		this.translatedCaption = translatedCaption;
+	}
+
+	public Set<String> createHashtag() {
+		Set<String> hashtagSet = new HashSet<>();
+
+		Pattern pattern = Pattern.compile("#([가-힣a-zA-Z0-9_]+)");
+		Matcher matcher = pattern.matcher(this.caption);
+
+		while (matcher.find()) {
+			hashtagSet.add(matcher.group(1));
+		}
+		return hashtagSet;
 	}
 }
