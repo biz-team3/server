45730d4bcee41b40e05330574e705c411ea61145
qui-0 <molok41817@gmail.com>
2026-05-11 17:35:54 +0900

    feat: implement access token check and authentication interceptor

--- src/main/java/com/bizteam3/server/global/config/WebConfig.java
+++ src/main/java/com/bizteam3/server/global/config/WebConfig.java
@@ -2,7 +2,9 @@ package com.bizteam3.server.global.config;
 
 import java.nio.file.Paths;
 
+import com.bizteam3.server.global.auth.interceptor.AuthInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -15,6 +17,7 @@ import lombok.RequiredArgsConstructor;
 public class WebConfig implements WebMvcConfigurer {
 
 	private final FileUploadProperties properties;
+	private final AuthInterceptor authInterceptor;
 
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
@@ -25,5 +28,11 @@ public class WebConfig implements WebMvcConfigurer {
 
 		registry.addResourceHandler(properties.getUrlPrefix() + "/**")
 			.addResourceLocations("file:" + uploadPath + "/");
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(authInterceptor)
+				.addPathPatterns("/api/**");
 	}
 }
