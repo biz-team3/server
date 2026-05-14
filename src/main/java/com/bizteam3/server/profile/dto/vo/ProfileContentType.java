48486d078e68cfe65741782f02e7d2d7640e0149
kwongwangjae <121513336+kwongwangjae@users.noreply.github.com>
2026-05-14 12:07:56 +0900

    feat:saved-posts (#59)

---
+++ src/main/java/com/bizteam3/server/profile/dto/vo/ProfileContentType.java
@@ -0,0 +1,7 @@
+package com.bizteam3.server.profile.dto.vo;
+
+public enum ProfileContentType {
+	POSTS,
+	SAVED
+}
+
--- src/main/java/com/bizteam3/server/save/dao/SaveDao.java
+++ src/main/java/com/bizteam3/server/save/dao/SaveDao.java
@@ -1,5 +1,8 @@
 package com.bizteam3.server.save.dao;
 
+import java.util.List;
+
+import com.bizteam3.server.post.entity.Post;
 import com.bizteam3.server.save.entity.Save;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
@@ -21,6 +24,16 @@ public interface SaveDao {
     int isSaved(
             @Param("userId") Integer userId,
             @Param("postId") Integer postId
+    );
+
+    List<Post> selectFeedPostsByUserId(
+        @Param("userId") Integer userId,
+        @Param("offset") int offset,
+        @Param("size") int size
+    );
+
+    int countAllByUserId(
+        @Param("userId") Integer userId
     );
 
 }
