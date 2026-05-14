48f7ccad61e2deec1828da7a98ac78a065fa8a77
kwongwangjae <121513336+kwongwangjae@users.noreply.github.com>
2026-05-14 15:38:42 +0900

    hotfix (#62)

---
+++ src/test/java/com/bizteam3/server/user/service/UserServiceImplTest.java
@@ -0,0 +1,12 @@
+package com.bizteam3.server.user.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class UserServiceImplTest {
+
+}
